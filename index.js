ece025c7a2049d49e6155625734dfa74df006701
Jonatas Lima Araújo <johnlima540@gmail.com>
2026-02-05 11:05:30 -0500

    novo tuor

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ function changeView(view) {
   const iframe = document.getElementById("tourFrame");
 
   const views = {
-    entrada: "https://my.matterport.com/show/?m=wdSbDmbX75F&play=1&portal=1&ss=2&sr=-1.65,.97",
-    musculacao: "https://my.matterport.com/show/?m=wdSbDmbX75F&play=1&portal=1&ss=5&sr=-1.13,.49&play=1&portal=1",
-    inicio: "https://my.matterport.com/show/?m=wdSbDmbX75F&play=1&portal=1"
-  };
+  inicio: "https://my.matterport.com/show/?m=32hoyJfC63W&play=1&brand=0",
+  entrada: "https://my.matterport.com/show/?m=32hoyJfC63W&ss=2&sr=-2.79,-.13&brand=0&play=1&portal=1&brand=0",
+  corredor01: "https://my.matterport.com/show/?m=32hoyJfC63W&ss=3&sr=-2.01,-1.09&brand=0&play=1&portal=1&brand=0",
+  corredor02: "https://my.matterport.com/show/?m=32hoyJfC63W&ss=4&sr=-2.01,-1.09&brand=0&play=1&portal=1&brand=0",
+  corredor03: "https://my.matterport.com/show/?m=32hoyJfC63W&ss=11&sr=-2.54,-.05&brand=0&play=1&portal=1&brand=0",
+  musculacao01: "https://my.matterport.com/show/?m=32hoyJfC63W&ss=10&sr=-2.54,-.05&brand=0&play=1&portal=1&brand=0",
+  esteiras: "https://my.matterport.com/show/?m=32hoyJfC63W&ss=6&sr=-.85,.21&brand=0&play=1&portal=1&brand=0",
+};
 
   iframe.src = views[view];
 }
